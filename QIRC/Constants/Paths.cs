ea8be37a27de12bcf2efbe5f0b166d229a25c505
Thomas <dorianstoll@googlemail.com>
2015-12-21 20:40:00 +0100

    Made Path standalone

--- QIRC/Constants/Paths.cs
+++ QIRC/Constants/Paths.cs
@@ -33,59 +33,9 @@ namespace QIRC.Constants
         public static readonly Path logs = "logs/";
 
         /// <summary>
-        /// Path wrapper to return the current Directory
+        /// The directory where the plugins are stored.
+        /// This is relative to the location of the main executable.
         /// </summary>
-        public class Path
-        {
-            /// <summary>
-            /// The respective path, relative to the main executable.
-            /// </summary>
-            public String name { get; set; }
-
-            /// <summary>
-            /// Create the path object.
-            /// </summary>
-            /// <param name="name">See <see cref="name"/></param>
-            public Path(String name)
-            {
-                this.name = name;
-            }
-
-            /// <summary>
-            /// Returns if the given path exists
-            /// </summary>
-            public Boolean Exists()
-            {
-                return File.Exists(PathIO.Combine(Directory.GetCurrentDirectory(), name));
-            }
-
-            /// <summary>
-            /// Returns if the given path exists.
-            /// This one has an additional check for a file that is located at the path.
-            /// </summary>
-            public Boolean Exists(String file)
-            {
-                return File.Exists(PathIO.Combine(Directory.GetCurrentDirectory(), name, file));
-            }
-
-            /// <summary>
-            /// Converts the path into a string representation of the (absolute) path.
-            /// </summary>
-            public static implicit operator String(Path path)
-            {
-                String p = PathIO.Combine(Directory.GetCurrentDirectory(), path.name);
-                return p;
-            }
-
-            /// <summary>
-            /// Converts a string into a path representation.
-            /// </summary>
-            public static implicit operator Path(String path)
-            {
-                if (path.StartsWith(Directory.GetCurrentDirectory()))
-                    path = path.Remove(0, Directory.GetCurrentDirectory().Length);
-                return new Path(path);
-            }
-        }
+        public static readonly Path plugins = "plugins/";
     }
 }
---
+++ QIRC/Path.cs
@@ -0,0 +1,72 @@
+/// --------------------------------------
+/// .NET Bot for Internet Relay Chat (IRC)
+/// Copyright (c) ThomasKerman 2015
+/// QIRC is licensed under the MIT License
+/// --------------------------------------
+
+/// System
+using System;
+using System.IO;
+using PathIO = System.IO.Path;
+
+/// <summary>
+/// The main namespace. Here's everything that executes actively.
+/// </summary>
+namespace QIRC
+{
+    /// <summary>
+    /// Path wrapper to return the current Directory
+    /// </summary>
+    public class Path
+    {
+        /// <summary>
+        /// The respective path, relative to the main executable.
+        /// </summary>
+        public String name { get; set; }
+
+        /// <summary>
+        /// Create the path object.
+        /// </summary>
+        /// <param name="name">See <see cref="name"/></param>
+        public Path(String name)
+        {
+            this.name = name;
+        }
+
+        /// <summary>
+        /// Returns if the given path exists
+        /// </summary>
+        public Boolean Exists()
+        {
+            return File.Exists(PathIO.Combine(Directory.GetCurrentDirectory(), name));
+        }
+
+        /// <summary>
+        /// Returns if the given path exists.
+        /// This one has an additional check for a file that is located at the path.
+        /// </summary>
+        public Boolean Exists(String file)
+        {
+            return File.Exists(PathIO.Combine(Directory.GetCurrentDirectory(), name, file));
+        }
+
+        /// <summary>
+        /// Converts the path into a string representation of the (absolute) path.
+        /// </summary>
+        public static implicit operator String(Path path)
+        {
+            String p = PathIO.Combine(Directory.GetCurrentDirectory(), path.name);
+            return p;
+        }
+
+        /// <summary>
+        /// Converts a string into a path representation.
+        /// </summary>
+        public static implicit operator Path(String path)
+        {
+            if (path.StartsWith(Directory.GetCurrentDirectory()))
+                path = path.Remove(0, Directory.GetCurrentDirectory().Length);
+            return new Path(path);
+        }
+    }
+}
