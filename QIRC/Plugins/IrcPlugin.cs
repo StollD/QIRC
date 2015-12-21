9e071695453f3da7c62fbb4db345cff81532b47c
Thomas <dorianstoll@googlemail.com>
2015-12-21 20:40:17 +0100

    Added PluginManager and IrcPlugin

---
+++ QIRC/Plugins/IrcPlugin.cs
@@ -0,0 +1,25 @@
+﻿/// --------------------------------------
+/// .NET Bot for Internet Relay Chat (IRC)
+/// Copyright (c) ThomasKerman 2015
+/// QIRC is licensed under the MIT License
+/// --------------------------------------
+
+/// System
+using System;
+
+/// <summary>
+/// The namespace where everything Plugin related is stored.
+/// </summary>
+namespace QIRC.Plugins
+{
+    /// <summary>
+    /// This is an abstract base for a QIRC Plugin
+    /// </summary>
+    public abstract class IrcPlugin
+    {
+        public abstract String GetName();
+        public abstract String GetInfo();
+        public abstract String GetVersion();
+    }
+}
+
---
+++ QIRC/Plugins/PluginManager.cs
@@ -0,0 +1,76 @@
+﻿/// --------------------------------------
+/// .NET Bot for Internet Relay Chat (IRC)
+/// Copyright (c) ThomasKerman 2015
+/// QIRC is licensed under the MIT License
+/// --------------------------------------
+
+/// QIRC
+using QIRC.Constants;
+
+/// System
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+/// <summary>
+/// The namespace where everything Plugin related is stored.
+/// </summary>
+namespace QIRC.Plugins
+{
+    /// <summary>
+    /// This class loads and manages plugins for QIRC
+    /// </summary>
+    public class PluginManager
+    {
+        /// <summary>
+        /// All loaded <see cref="IrcPlugin"/>
+        /// </summary>
+        public static HashSet<IrcPlugin> plugins { get; set; }
+
+        /// <summary>
+        /// All loaded <see cref="Assembly"/>
+        /// </summary>
+        public static HashSet<Assembly> assemblies { get; set; }
+
+        /// <summary>
+        /// Loads the <see cref="Assembly"/> at the given Path
+        /// </summary>
+        public static Assembly LoadAssembly(Path path)
+        {
+            if (!path.Exists())
+                throw new ArgumentException("The path \"" + path + "\" doesn't exist!", nameof(path));
+            Byte[] buffer = File.ReadAllBytes(path);
+            Assembly assembly = AppDomain.CurrentDomain.Load(buffer);
+            assemblies.Add(assembly);
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (type.IsSubclassOf(typeof(IrcPlugin)))
+                {
+                    IrcPlugin plugin = (IrcPlugin)Activator.CreateInstance(type);
+                    plugins.Add(plugin);
+                }
+            }
+            return assembly;
+        }
+
+        /// <summary>
+        /// Loads the .dll files from the Plugins directory
+        /// </summary>
+        public static void Load()
+        {
+            foreach (String file in Directory.GetFiles(Paths.plugins, "*.dll"))
+                LoadAssembly(file);
+        }
+
+        /// <summary>
+        /// Initialization
+        /// </summary>
+        static PluginManager()
+        {
+            assemblies = new HashSet<Assembly>();
+            plugins = new HashSet<IrcPlugin>();
+            Directory.CreateDirectory(Paths.plugins);
+        }
+    }
+}
