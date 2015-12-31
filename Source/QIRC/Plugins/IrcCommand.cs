6cd0138e5ecaf10dd5876029e3a447895a76923d
Thomas <dorianstoll@googlemail.com>
2015-12-31 15:19:02 +0100

    Added Parameter Utils to IrcCommand

--- Source/QIRC/Plugins/IrcCommand.cs
+++ Source/QIRC/Plugins/IrcCommand.cs
@@ -29,6 +29,8 @@ namespace QIRC.Plugins
         public virtual AccessLevel GetAccessLevel() { return AccessLevel.NORMAL; }
         public virtual void RunCommand(IrcClient client, ProtoIrcMessage message) { }
         public virtual String[] GetParameters() { return new String[0]; }
+        protected Boolean StartsWithParam(String param, String message) { return message.StartsWith("-" + param + "=") || message.StartsWith("-" + param + ":"); }
+        protected String StripParam(String param, String message) { return message.Replace("-" + param + "=", "").Replace("-" + param + ":", ""); }
     }
 }
 
