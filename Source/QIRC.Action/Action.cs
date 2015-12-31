a8af80e28959b6a382112d74e9c5bc29f5463aa5
Thomas <dorianstoll@googlemail.com>
2015-12-31 15:20:08 +0100

    Added Action Command

---
+++ Source/QIRC.Action/Action.cs
@@ -0,0 +1,92 @@
+﻿/// --------------------------------------
+/// .NET Bot for Internet Relay Chat (IRC)
+/// Copyright (c) ThomasKerman 2015
+/// QIRC is licensed under the MIT License
+/// --------------------------------------
+
+/// IRC
+using ChatSharp;
+using ChatSharp.Events;
+
+/// QIRC
+using QIRC;
+using QIRC.IRC;
+using QIRC.Plugins;
+
+/// System
+using System;
+
+/// <summary>
+/// Here's everything that is an IrcCommand
+/// </summary>
+namespace QIRC.Commands
+{
+    /// <summary>
+    /// This is the implementation for the action command. The bot will grab the words
+    /// behind the control sequence and output it as an action. Pretty basic
+    /// </summary>
+    public class Action : IrcCommand
+    {
+        /// <summary>
+        /// The Access Level that is needed to execute the command
+        /// </summary>
+        public override AccessLevel GetAccessLevel()
+        {
+            return AccessLevel.ROOT;
+        }
+
+        /// <summary>
+        /// The name of the command
+        /// </summary>
+        public override String GetName()
+        {
+            return "action";
+        }
+
+        /// <summary>
+        /// Returns a description of the command
+        /// </summary>
+        public override String GetDescription()
+        {
+            return "Outputs the given text to the given channel as an Action.";
+        }
+
+        /// <summary>
+        /// The Parameters of the Command
+        /// </summary>
+        public override String[] GetParameters()
+        {
+            return new String[]
+            {
+                "to", "The Channel where the bot should send the action to."
+            };
+        }
+
+        /// <summary>
+        /// Whether the command can be used in serious channels.
+        /// </summary>
+        public override Boolean IsSerious()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Here we run the command and evaluate the parameters
+        /// </summary>
+        public override void RunCommand(IrcClient client, ProtoIrcMessage message)
+        {
+            if (StartsWithParam("to", message.Message))
+            {
+                message.Message = StripParam("to", message.Message);
+                String[] split = message.Message.Split(new[] { ' ' }, 2);
+                String target = split[0];
+                String text = split[1];
+                QIRC.SendAction(client, text, target);
+            }
+            else
+            {
+                QIRC.SendAction(client, message.Message, message.Source);
+            }
+        }
+    }
+}
---
+++ Source/QIRC.Action/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QIRC.Action")]
+[assembly: AssemblyDescription("Action Command for QIRC")]
+[assembly: AssemblyProduct("QIRC")]
+[assembly: AssemblyCopyright("Copyright © ThomasKerman 2015")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f70e226e-0e7f-4520-9407-57e9f861ce16")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
