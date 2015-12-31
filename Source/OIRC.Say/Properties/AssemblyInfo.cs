3d78c816a4789d3074b23949a09f1f24be8def8d
Thomas <dorianstoll@googlemail.com>
2015-12-31 15:19:58 +0100

    Added Say Command

---
+++ Source/OIRC.Say/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QIRC.Say")]
+[assembly: AssemblyDescription("Say Command for QIRC")]
+[assembly: AssemblyProduct("QIRC")]
+[assembly: AssemblyCopyright("Copyright © ThomasKerman 2015")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ce2c9d61-fa3c-4cc3-8338-d6319053414e")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
