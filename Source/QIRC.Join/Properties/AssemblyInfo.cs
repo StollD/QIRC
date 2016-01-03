4059c34ba5df653628e71511c5f95a589d1c86b8
Thomas <dorianstoll@googlemail.com>
2016-01-03 10:55:00 +0100

    Added the Join Command

---
+++ Source/QIRC.Join/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QIRC.Join")]
+[assembly: AssemblyDescription("Join Command for QIRC")]
+[assembly: AssemblyProduct("QIRC")]
+[assembly: AssemblyCopyright("Copyright © ThomasKerman 2015")]
+[assembly: ComVisible(false)]
+[assembly: Guid("046ab02d-59b4-4d56-8c85-c9918c364a25")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
