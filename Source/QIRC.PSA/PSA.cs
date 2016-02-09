e111e56966b400d9b60ce161fbe3f00941e6a607
Thomas <dorianstoll@googlemail.com>
2016-02-09 23:18:36 +0100

    Added PSA command

---
+++ Source/QIRC.PSA/PSA.cs
@@ -0,0 +1,84 @@
+﻿/// --------------------------------------
+/// .NET Bot for Internet Relay Chat (IRC)
+/// Copyright (c) ThomasKerman 2016
+/// QIRC is licensed under the MIT License
+/// --------------------------------------
+
+/// IRC
+using ChatSharp;
+using ChatSharp.Events;
+
+/// QIRC
+using QIRC;
+using QIRC.Configuration;
+using QIRC.IRC;
+using QIRC.Plugins;
+
+/// System
+using System;
+using System.Linq;
+
+/// <summary>
+/// Here's everything that is an IrcCommand
+/// </summary>
+namespace QIRC.Commands
+{
+    /// <summary>
+    /// This is the implementation for the psa command. The bot will post the given message
+    /// in every channel he has joined to inform end users about feature changes.
+    /// </summary>
+    public class PSA : IrcCommand
+    {
+        /// <summary>
+        /// The Access Level that is needed to execute the command
+        /// </summary>
+        public override AccessLevel GetAccessLevel()
+        {
+            return AccessLevel.ADMIN;
+        }
+
+        /// <summary>
+        /// The name of the command
+        /// </summary>
+        public override String GetName()
+        {
+            return "psa";
+        }
+
+        /// <summary>
+        /// Returns a description of the command
+        /// </summary>
+        public override String GetDescription()
+        {
+            return "Posts a public service announcement on all channels the bot has joined.";
+        }
+
+        /// <summary>
+        /// Whether the command can be used in serious channels.
+        /// </summary>
+        public override Boolean IsSerious()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// An example for using the command.
+        /// </summary>
+        /// <returns></returns>
+        public override String GetExample()
+        {
+            return Settings.Read<String>("control") + GetName() + " Warning! Due to server maintainance the bot will shut down in 5 minutes";
+        }
+
+        /// <summary>
+        /// Here we run the command and evaluate the parameters
+        /// </summary>
+        public override void RunCommand(IrcClient client, ProtoIrcMessage message)
+        {
+            foreach (IrcChannel channel in client.Channels)
+            {
+                QIRC.SendMessage(client, "[PSA] " + message.Message, Settings.Read<String>("name"), channel.Name, true);
+            }
+        }
+    }
+}
---
+++ Source/QIRC.PSA/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QIRC.PSA")]
+[assembly: AssemblyDescription("PSA Command for QIRC")]
+[assembly: AssemblyProduct("QIRC")]
+[assembly: AssemblyCopyright("Copyright © ThomasKerman 2016")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5f5444a0-342b-47e5-84f9-5c2da8ef2051")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
