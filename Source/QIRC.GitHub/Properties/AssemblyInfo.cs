10bacf845a85c51d2b04fd9fc2d7188d319c2dc3
Thomas <dorianstoll@googlemail.com>
2016-02-20 18:39:45 +0100

    Added GitHub integration

---
+++ Source/QIRC.GitHub/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QIRC.GitHub")]
+[assembly: AssemblyDescription("GitHub Functions for QIRC")]
+[assembly: AssemblyProduct("QIRC")]
+[assembly: AssemblyCopyright("Copyright © ThomasKerman 2016")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7ff76f13-1745-418e-a01d-0801d2bbb9b6")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
