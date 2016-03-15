c733609fcd425dc99076c88a4a52e14ba76e8dc0
Thomas <dorian@tmsp.ga>
2016-03-15 21:08:01 +0100

    ban System.Runtime

--- Source/QIRC.CSharp/CSharp.cs
+++ Source/QIRC.CSharp/CSharp.cs
@@ -172,6 +172,7 @@ namespace QIRC.Commands
             Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
             input = input.Replace("System.IO", "")
                 .Replace("System.Xml", "")
+                .Replace("System.Runtime", "")
                 .Replace("System.Diagnostics.Process", "")
                 .Replace("while(true)", "")
                 .Replace("while (true)", "")
