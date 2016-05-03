cac6a96e95028815c3b071664f5039fba6ba3b95
Thomas <dorian@tmsp.ga>
2016-05-03 21:07:28 +0200

    Overhauled it and added willies flags

--- Source/QIRC.FindReplace/FindReplace.cs
+++ Source/QIRC.FindReplace/FindReplace.cs
@@ -1,28 +1,17 @@
-﻿/// --------------------------------------
-/// .NET Bot for Internet Relay Chat (IRC)
-/// Copyright (c) ThomasKerman 2016
-/// QIRC is licensed under the MIT License
-/// --------------------------------------
+﻿/** 
+ * .NET Bot for Internet Relay Chat (IRC)
+ * Copyright (c) ThomasKerman 2016
+ * QIRC is licensed under the MIT License
+ */
 
-/// IRC
 using ChatSharp;
 using ChatSharp.Events;
-
-/// QIRC
-using QIRC;
-using QIRC.Configuration;
 using QIRC.IRC;
 using QIRC.Plugins;
-using QIRC.Serialization;
-
-/// System
 using System;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-/// <summary>
-/// Here's everything that logs to disk and CMD
-/// </summary>
 namespace QIRC.Addons
 {
     /// <summary>
@@ -34,28 +23,46 @@ namespace QIRC.Addons
         /// <summary>
         /// The regular expression that is used to detect the syntax (Copyright goes to the Willie/Sopel Devs)
         /// </summary>
-        public const String regex = @"^(?:(\S+)[:,]\s+)?s/((?:\\/|[^\/])+)/((?:\\/|[^\/])*)(?:/)?$";
+        public const String regex = @"(?:(\S+)[:,]\s+)?s/((?:\\/|[^/])+)/((?:\\/|[^/])*)(?:/(\S+))?";
 
         /// <summary>
         /// This gets invoked when someone wrote something in a channel
         /// </summary>
         public override void OnChannelMessageRecieved(IrcClient client, PrivateMessageEventArgs e)
         {
+            // Create a new message 
             ProtoIrcMessage message = new ProtoIrcMessage(e);
+
+            // If there is no match, we have nothing to do
             if (!Regex.IsMatch(message.Message, regex, RegexOptions.IgnoreCase))
                 return;
+
+            // Get the values from the message
             Match match = Regex.Match(message.Message, regex, RegexOptions.IgnoreCase);
-            String nick = match.Groups[1].Success ? match.Groups[1].Value : message.User;                
+            String nick = match.Groups[1].Success ? match.Groups[1].Value : message.User;
+            Console.WriteLine(nick);
             if (!client.Users.Contains(nick))
                 return;
             String find = match.Groups[2].Value.Replace(@"\/", "/");
             String repl = match.Groups[3].Value.Replace(@"\/", "/");
-            ProtoIrcMessage new_msg = QIRC.messages.Where(m => m.User == nick && Regex.IsMatch(m.Message, find, RegexOptions.IgnoreCase)).LastOrDefault();
+
+            // Find the message to edit
+            ProtoIrcMessage new_msg = QIRC.messages.LastOrDefault(m => m.User == nick && Regex.IsMatch(m.Message, find, RegexOptions.IgnoreCase));
+            Char[] flags = match.Groups[4].Success ? match.Groups[4].Value.ToCharArray() : new Char[0];
+            Console.WriteLine(new string(flags));
             if (new_msg == null)
                 return;
-            if (new_msg.Message.StartsWith("\x01" + "ACTION"))
-                new_msg.Message = "/me" + new_msg.Message.Remove(0, ("\x01" + "ACTION").Length - 1).Remove(new_msg.Message.Length - 2);
-            new_msg.Message = Regex.Replace(new_msg.Message, find, repl, RegexOptions.IgnoreCase);
+            if (new_msg.Message.StartsWith("ACTION"))
+                new_msg.Message = "/me" + new_msg.Message.Remove(0, "ACTION".Length - 1);
+
+            // Regex options
+            RegexOptions options = flags.Contains('i') || flags.Contains('I') ? RegexOptions.IgnoreCase : RegexOptions.None;
+            Regex rFind = new Regex(find, options);
+
+            // Replace stuff
+            new_msg.Message = flags.Contains('g') || flags.Contains('G') ? Regex.Replace(new_msg.Message, find, repl, options) : rFind.Replace(new_msg.Message, repl, 1);
+
+            // Send the message back
             if (nick == message.User)
                 QIRC.SendMessage(client, nick + " [b]meant[/b] to say: " + new_msg.Message, message.User, message.Source, true);
             else if (!client.Channels[message.Source].Users.Contains("Kountdown"))
