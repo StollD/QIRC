7b3d7c97aeae4c06cb965132830891ccfff54553
Dorian Stoll <dorian@tmsp.ga>
2016-10-27 04:41:22 +0200

    Fix data saving

--- Source/QIRC/Serialization/SerializeableList.cs
+++ Source/QIRC/Serialization/SerializeableList.cs
@@ -13,6 +13,8 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.IO.Compression;
+using System.Threading;
+using QIRC.Configuration;
 
 namespace QIRC.Serialization
 {
@@ -22,19 +24,44 @@ namespace QIRC.Serialization
     /// <typeparam name="T"></typeparam>
     public class SerializeableList<T> : IList<T>, ICollection<T>, IEnumerable<T>, IEnumerable, IList, ICollection, IReadOnlyList<T>, IReadOnlyCollection<T>
     {
-        protected List<T> data { get; set; }
+        protected Object lockObj = new Object();
+
+        protected List<T> _data;
+
+        protected List<T> data
+        {
+            get
+            {
+                lock (lockObj)
+                    return _data;
+            }
+            set
+            {
+                lock (lockObj)
+                    _data = value;
+            }
+        }
 
         protected String name { get; set; }
 
         public Int32 Capacity
         {
-            get { return data.Capacity; }
-            set { data.Capacity = value; Save(); }
+            get
+            {
+                lock (lockObj) return data.Capacity;
+            }
+            set
+            {
+                lock (lockObj) data.Capacity = value;
+            }
         }
 
         public Int32 Count
         {
-            get { return data.Count; }
+            get
+            {
+                lock (lockObj) return data.Count;
+            }
         }
 
         Boolean IList.IsFixedSize
@@ -54,27 +81,39 @@ namespace QIRC.Serialization
 
         Boolean ICollection.IsSynchronized
         {
-            get
-            {
-                return false;
-            }
+            get { return false; }
         }
 
         Object ICollection.SyncRoot
         {
-            get { return (data as ICollection).SyncRoot;}
+            get
+            {
+                lock (lockObj) return (data as ICollection).SyncRoot;
+            }
         }
 
         public T this[Int32 index]
         {
-            get { return data[index]; }
-            set { data[index] = value; Save(); }
+            get
+            {
+                lock (lockObj) return data[index];
+            }
+            set
+            {
+                lock (lockObj) data[index] = value;
+            }
         }
 
         Object IList.this[Int32 index]
         {
-            get { return data[index]; }
-            set { data[index] = (T)value; Save(); }
+            get
+            {
+                lock (lockObj) return data[index];
+            }
+            set
+            {
+                lock (lockObj) data[index] = (T) value;
+            }
         }
 
         public SerializeableList(String name)
@@ -91,8 +130,10 @@ namespace QIRC.Serialization
             else
             {
                 data = new List<T>();
-                Save();
             }
+            Thread save = new Thread(Save);
+            save.IsBackground = true;
+            save.Start();
         }
 
         public SerializeableList(String name, Int32 capacity)
@@ -110,8 +151,10 @@ namespace QIRC.Serialization
             else
             {
                 data = new List<T>(capacity);
-                Save();
             }
+            Thread save = new Thread(Save);
+            save.IsBackground = true;
+            save.Start();
         }
 
         public SerializeableList(String name, IEnumerable<T> collection)
@@ -129,306 +172,346 @@ namespace QIRC.Serialization
             else
             {
                 data = new List<T>(collection);
-                Save();
             }
+            Thread save = new Thread(Save);
+            save.IsBackground = true;
+            save.Start();
         }
 
         public void Add(T item)
         {
-            data.Add(item);
-            Save();
+            lock (lockObj)
+                data.Add(item);
         }
 
         Int32 IList.Add(Object item)
         {
-            Int32 i = (data as IList).Add(item);
-            Save();
-            return i;
+            lock (lockObj)
+                return (data as IList).Add(item);
         }
 
         public void AddRange(IEnumerable<T> collection)
         {
-            data.AddRange(collection);
-            Save();
+            lock (lockObj)
+                data.AddRange(collection);
         }
 
         public ReadOnlyCollection<T> AsReadOnly()
         {
-            return data.AsReadOnly();
+            lock (lockObj)
+                return data.AsReadOnly();
         }
 
         public Int32 BinarySearch(Int32 index, Int32 count, T item, IComparer<T> comparer)
         {
-            return data.BinarySearch(index, count, item, comparer);
+            lock (lockObj)
+                return data.BinarySearch(index, count, item, comparer);
         }
 
         public Int32 BinarySearch(T item)
         {
-            return data.BinarySearch(0, data.Count, item, null);
+            lock (lockObj)
+                return data.BinarySearch(0, data.Count, item, null);
         }
 
         public Int32 BinarySearch(T item, IComparer<T> comparer)
         {
-            return data.BinarySearch(0, data.Count, item, comparer);
+            lock (lockObj)
+                return data.BinarySearch(0, data.Count, item, comparer);
         }
 
         public void Clear()
         {
-            data.Clear();
-            Save();
+            lock (lockObj)
+                data.Clear();
         }
 
         public Boolean Contains(T item)
         {
-            return data.Contains(item);
+            lock (lockObj)
+                return data.Contains(item);
         }
 
         Boolean IList.Contains(Object item)
         {
-            return (data as IList).Contains(item);
+            lock (lockObj)
+                return (data as IList).Contains(item);
         }
 
         public List<TOutput> ConvertAll<TOutput>(Converter<T, TOutput> converter)
         {
-            return data.ConvertAll(converter);
+            lock (lockObj)
+                return data.ConvertAll(converter);
         }
 
         public void CopyTo(T[] array)
         {
-            data.CopyTo(array, 0);
+            lock (lockObj)
+                data.CopyTo(array, 0);
         }
 
         void ICollection.CopyTo(Array array, Int32 arrayIndex)
         {
-            (data as ICollection).CopyTo(array, arrayIndex);
+            lock (lockObj)
+                (data as ICollection).CopyTo(array, arrayIndex);
         }
 
         public void CopyTo(Int32 index, T[] array, Int32 arrayIndex, Int32 count)
         {
-            data.CopyTo(index, array, arrayIndex, count);
+            lock (lockObj)
+                data.CopyTo(index, array, arrayIndex, count);
         }
 
         public void CopyTo(T[] array, Int32 arrayIndex)
         {
-            data.CopyTo(array, arrayIndex);
+            lock (lockObj)
+                data.CopyTo(array, arrayIndex);
         }
 
         public Boolean Exists(Predicate<T> match)
         {
-            return data.Exists(match);
+            lock (lockObj)
+                return data.Exists(match);
         }
 
         public T Find(Predicate<T> match)
         {
-            return data.Find(match);
+            lock (lockObj)
+                return data.Find(match);
         }
 
         public List<T> FindAll(Predicate<T> match)
         {
-            return data.FindAll(match);
+            lock (lockObj)
+                return data.FindAll(match);
         }
 
         public Int32 FindIndex(Predicate<T> match)
         {
-            return data.FindIndex(match);
+            lock (lockObj)
+                return data.FindIndex(match);
         }
 
         public Int32 FindIndex(Int32 startIndex, Predicate<T> match)
         {
-            return data.FindIndex(startIndex, match);
+            lock (lockObj)
+                return data.FindIndex(startIndex, match);
         }
 
         public Int32 FindIndex(Int32 startIndex, Int32 count, Predicate<T> match)
         {
-            return data.FindIndex(startIndex, count, match);
+            lock (lockObj)
+                return data.FindIndex(startIndex, count, match);
         }
 
         public T FindLast(Predicate<T> match)
         {
-            return data.FindLast(match);
+            lock (lockObj)
+                return data.FindLast(match);
         }
 
         public Int32 FindLastIndex(Predicate<T> match)
         {
-            return data.FindLastIndex(match);
+            lock (lockObj)
+                return data.FindLastIndex(match);
         }
 
         public Int32 FindLastIndex(Int32 startIndex, Predicate<T> match)
         {
-            return data.FindLastIndex(startIndex, match);
+            lock (lockObj)
+                return data.FindLastIndex(startIndex, match);
         }
 
         public Int32 FindLastIndex(Int32 startIndex, Int32 count, Predicate<T> match)
         {
-            return data.FindLastIndex(startIndex, count, match);
+            lock (lockObj)
+                return data.FindLastIndex(startIndex, count, match);
         }
 
         public void ForEach(Action<T> action)
         {
-            data.ForEach(action);
+            lock (lockObj)
+                data.ForEach(action);
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            return data.GetEnumerator();
+            lock (lockObj)
+                return data.GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return data.GetEnumerator();
+            lock (lockObj)
+                return data.GetEnumerator();
         }
 
         public List<T> GetRange(Int32 index, Int32 count)
         {
-            return data.GetRange(index, count);
+            lock (lockObj)
+                return data.GetRange(index, count);
         }
 
         public Int32 IndexOf(T item)
         {
-            return data.IndexOf(item);
+            lock (lockObj)
+                return data.IndexOf(item);
         }
 
         Int32 IList.IndexOf(Object item)
         {
-            return (data as IList).IndexOf(item);
+            lock (lockObj)
+                return (data as IList).IndexOf(item);
         }
 
         public Int32 IndexOf(T item, Int32 index)
         {
-            return data.IndexOf(item, index);
+            lock (lockObj)
+                return data.IndexOf(item, index);
         }
 
         public Int32 IndexOf(T item, Int32 index, Int32 count)
         {
-            return data.IndexOf(item, index, count);
+            lock (lockObj)
+                return data.IndexOf(item, index, count);
         }
 
         public void Insert(Int32 index, T item)
         {
-            data.Insert(index, item);
-            Save();
+            lock (lockObj)
+                data.Insert(index, item);
         }
 
         void IList.Insert(Int32 index, Object item)
         {
-            (data as IList).Insert(index, item);
-            Save();
+            lock (lockObj)
+                (data as IList).Insert(index, item);
         }
 
         public void InsertRange(Int32 index, IEnumerable<T> collection)
         {
-            data.InsertRange(index, collection);
-            Save();
+            lock (lockObj)
+                data.InsertRange(index, collection);
         }
 
         public Int32 LastIndexOf(T item)
         {
-            return data.LastIndexOf(item);
+            lock (lockObj)
+                return data.LastIndexOf(item);
         }
 
         public Int32 LastIndexOf(T item, Int32 index)
         {
-            return data.LastIndexOf(item, index);
+            lock (lockObj)
+                return data.LastIndexOf(item, index);
         }
 
         public Int32 LastIndexOf(T item, Int32 index, Int32 count)
         {
-            return data.LastIndexOf(item, index, count);
+            lock (lockObj)
+                return data.LastIndexOf(item, index, count);
         }
 
         public Boolean Remove(T item)
         {
-            Boolean b = data.Remove(item);
-            Save();
-            return b;
+            lock (lockObj)
+                return data.Remove(item);
         }
 
         void IList.Remove(Object item)
         {
-            (data as IList).Remove(item);
-            Save();
+            lock (lockObj)
+                (data as IList).Remove(item);
         }
 
         public Int32 RemoveAll(Predicate<T> match)
         {
-            Int32 i = data.RemoveAll(match);
-            Save();
-            return i;
+            lock (lockObj)
+                return data.RemoveAll(match);
         }
 
         public void RemoveAt(Int32 index)
         {
-            data.RemoveAt(index);
-            Save();
+            lock (lockObj)
+                data.RemoveAt(index);
         }
 
         public void RemoveRange(Int32 index, Int32 count)
         {
-            data.RemoveRange(index, count);
-            Save();
+            lock (lockObj)
+                data.RemoveRange(index, count);
         }
 
         public void Reverse()
         {
-            data.Reverse();
-            Save();
+            lock (lockObj)
+                data.Reverse();
         }
 
         public void Reverse(Int32 index, Int32 count)
         {
-            data.Reverse(index, count);
-            Save();
+            lock (lockObj)
+                data.Reverse(index, count);
         }
 
         public void Sort()
         {
-            data.Sort();
-            Save();
+            lock (lockObj)
+                data.Sort();
         }
 
         public void Sort(IComparer<T> comparer)
         {
-            data.Sort(comparer);
-            Save();
+            lock (lockObj)
+                data.Sort(comparer);
         }
 
         public void Sort(Int32 index, Int32 count, IComparer<T> comparer)
         {
-            data.Sort(index, count, comparer);
-            Save();
+            lock (lockObj)
+                data.Sort(index, count, comparer);
         }
 
         public void Sort(Comparison<T> comparison)
         {
-            data.Sort(comparison);
-            Save();
+            lock (lockObj)
+                data.Sort(comparison);
         }
 
         public T[] ToArray()
         {
-            return data.ToArray();
+            lock (lockObj)
+                return data.ToArray();
         }
 
         public void TrimExcess()
         {
-            data.TrimExcess();
-            Save();
+            lock (lockObj)
+                data.TrimExcess();
         }
 
         public Boolean TrueForAll(Predicate<T> match)
         {
-            return data.TrueForAll(match);
+            lock (lockObj)
+                return data.TrueForAll(match);
         }
 
         private void Save()
         {
-            Directory.CreateDirectory(Paths.data + "shadow/");
-            using (DeflateStream stream = new DeflateStream(File.Open(Paths.data + "shadow/" + name + ".dat", FileMode.Create), CompressionMode.Compress, false))
+            while (true)
             {
-                using (BsonWriter bson = new BsonWriter(stream))
-                    new JsonSerializer().Serialize(bson, data, typeof (List<T>));
+                Thread.Sleep(Settings.Read<Int32>("saveInterval") * 60 * 1000);
+                lock (lockObj)
+                {
+                    Directory.CreateDirectory(Paths.data + "shadow/");
+                    using (DeflateStream stream = new DeflateStream(File.Open(Paths.data + "shadow/" + name + ".dat", FileMode.Create), CompressionMode.Compress, false))
+                    {
+                        using (BsonWriter bson = new BsonWriter(stream))
+                            new JsonSerializer().Serialize(bson, data, typeof(List<T>));
+                    }
+                    File.Copy(Paths.data + "shadow/" + name + ".dat", Paths.data + name + ".dat", true);
+                }
             }
-            File.Copy(Paths.data + "shadow/" + name + ".dat", Paths.data + name + ".dat", true);
         }
     }
 }
