c8b8bac8e285e9b373974d418ec2a6e5d307f073
Dorian Stoll <dorian@tmsp.ga>
2016-11-01 16:59:49 +0100

    Channel editing command

---
+++ Source/QIRC.Channel/Channel.cs
@@ -0,0 +1,108 @@
+﻿/** 
+ * .NET Bot for Internet Relay Chat (IRC)
+ * Copyright (c) ThomasKerman 2016
+ * QIRC is licensed under the MIT License
+ */
+
+using ChatSharp;
+using QIRC.Configuration;
+using QIRC.IRC;
+using QIRC.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QIRC.Commands
+{
+    /// <summary>
+    /// This command provides channel settings for QIRC
+    /// </summary>
+    public class Channel : IrcCommand
+    {
+        /// <summary>
+        /// The Access Level that is needed to execute the command
+        /// </summary>
+        public override AccessLevel GetAccessLevel()
+        {
+            return AccessLevel.ADMIN;
+        }
+
+        /// <summary>
+        /// The name of the command
+        /// </summary>
+        public override String GetName()
+        {
+            return "channel";
+        }
+
+        /// <summary>
+        /// Returns a description of the command
+        /// </summary>
+        public override String GetDescription()
+        {
+            return "Edits the settings for the channel";
+        }
+
+        /// <summary>
+        /// Whether the command can be used in serious channels.
+        /// </summary>
+        public override Boolean IsSerious()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// The Parameters of the Command
+        /// </summary>
+        public override String[] GetParameters()
+        {
+            return new String[]
+            {
+                "serious", "If the channel should be serious or not",
+                "secret", "If the channel should be hidden.",
+                "state", "Displays the settings for this channel"
+            };
+        }
+
+        /// <summary>
+        /// An example for using the command.
+        /// </summary>
+        /// <returns></returns>
+        public override String GetExample()
+        {
+            return Settings.Read<String>("control") + GetName() + " -secret:true";
+        }
+
+        /// <summary>
+        /// Here we run the command and evaluate the parameters
+        /// </summary>
+        public override void RunCommand(IrcClient client, ProtoIrcMessage message)
+        {
+            // Check if this is a private message
+            if (!message.IsChannelMessage)
+            {
+                QIRC.SendMessage(client, "This command doesn't work in PM", message.User, message.Source, true);
+                return;
+            }
+
+            // Get the channel
+            List<ProtoIrcChannel> list = Settings.Read<List<ProtoIrcChannel>>("channels");
+            ProtoIrcChannel channel = list.Find(c => String.Equals(c.name, message.Source, StringComparison.InvariantCultureIgnoreCase));
+            String msg = message.Message;
+            if (StartsWithParam("serious", msg))
+            {
+                channel.serious = Boolean.Parse(StripParam("serious", ref msg));
+            }
+            if (StartsWithParam("secret", msg))
+            {
+                channel.secret = Boolean.Parse(StripParam("secret", ref msg));
+            }
+            if (StartsWithParam("state", msg))
+            {
+                QIRC.SendMessage(client, $"Serious: {channel.serious}, Secret: {channel.secret}", message.User, message.Source);
+            }
+            list[list.IndexOf(channel)] = channel;
+            Settings.Write("channels", list);
+        }
+    }
+}
---
+++ Source/QIRC.Channel/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QIRC.Channel")]
+[assembly: AssemblyDescription("Channel settings for QIRC")]
+[assembly: AssemblyProduct("QIRC")]
+[assembly: AssemblyCopyright("Copyright © ThomasKerman 2016")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c1c1dd53-e0b3-4748-a1eb-bcfb2bef03f5")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
